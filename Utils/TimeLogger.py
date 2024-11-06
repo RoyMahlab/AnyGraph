aa168e0f60053a1cab74c97c7e4dfaa17de04e9c
roy mahlab <roymahlab@gmail.com>
2024-11-06 18:01:05 +0200

    added function to present time normally

--- Utils/TimeLogger.py
+++ Utils/TimeLogger.py
@@ -22,6 +22,12 @@ def marktime(marker):
 	global timemark
 	timemark[marker] = datetime.datetime.now()
 
+def get_human_like_epoch_run_time(elapsed_time) -> tuple:
+	hours = int(elapsed_time // 3600)
+	minutes = int((elapsed_time % 3600) // 60)
+	seconds = int(elapsed_time % 60)
+	milliseconds = int((elapsed_time * 1000) % 1000)
+	return hours, minutes, seconds ,milliseconds
 
 if __name__ == '__main__':
 	log('')
